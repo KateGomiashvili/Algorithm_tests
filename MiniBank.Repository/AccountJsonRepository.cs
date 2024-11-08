a93782a78afbbccadc655b58efe819f8359c5e60
keti <gomiashviliketi@gmail.com>
2024-11-08 23:31:34 +0400

    added tests

---
+++ MiniBank.Repository/AccountJsonRepository.cs
@@ -0,0 +1,77 @@
+﻿using MiniBank.Models;
+using System.Security.Principal;
+using System.Text.Json;
+
+namespace MiniBank.Repository
+{
+    public class AccountJsonRepository
+    {
+        private readonly string _filePath;
+        private readonly List<Account> _accounts;
+
+        public AccountJsonRepository(string filePath)
+        {
+            _filePath = filePath;
+            _accounts = LoadData();
+        }
+
+        public List<Account> GetAccounts() => _accounts;
+
+
+        public List<Account> GetAccounts(int id) => _accounts.Where(account => account.Id == id).ToList();
+
+
+
+        public Account GetAccount(int id) => _accounts.FirstOrDefault(account => account.Id == id);
+
+
+        public void Create(Account account)
+        {
+            if (_accounts.All(c => c.Id != account.Id))
+            {
+                _accounts.Add(account);
+                SaveData();
+            }
+        }
+
+        public void Update(Account account)
+        {
+            var currentAccount = _accounts.FirstOrDefault(account=> account.Id == account.Id);
+            if (currentAccount != null) { 
+               currentAccount.Id = account.Id;
+               currentAccount.Name = account.Name;
+               currentAccount.Currency = account.Currency;
+               currentAccount.Balance = account.Balance;
+               currentAccount.Iban = account.Iban;
+               currentAccount.Name= account.Name;
+            }
+            SaveData();
+        }
+
+        public void Delete(int id)
+        {
+            var removedCustomer = _accounts.FirstOrDefault(c => c.Id == id);
+            if (removedCustomer != null)
+            {
+                _accounts.Remove(removedCustomer);
+                SaveData();
+            }
+        }
+
+        private void SaveData()
+        {
+            string jsonFile = JsonSerializer.Serialize(_accounts);
+            File.WriteAllText(_filePath, jsonFile);
+        }
+
+        private List<Account> LoadData()
+        {
+            if (!File.Exists(_filePath))
+            {
+                return new List<Account>();
+            }
+            var jsonData = File.ReadAllText(_filePath);
+            return JsonSerializer.Deserialize<List<Account>>(jsonData);
+        }
+    }
+}
---
+++ MiniBank.Tests/MiniBank_Json_Repository_Should.cs
@@ -0,0 +1,67 @@
+﻿using MiniBank.Models;
+using MiniBank.Repository;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+using System.Text.Json;
+
+
+namespace MiniBank.Tests
+{
+    public class MiniBank_Json_Repository_Should
+    {
+        [Fact]
+        public void Get_All_Accounts()
+        {
+            //Arrange
+            var accountRepository = new AccountJsonRepository(@"../../../../MiniBank.Tests/Data/Accounts.json");
+            var expected = 4;
+
+            //Act
+            var actual = accountRepository.GetAccounts();
+
+            //Assert
+            Assert.Equal(expected, actual.Count);
+        }
+        [Fact]
+        public void Get_Account_with_specific_Id()
+        {
+            //Arrange
+            var accountRepository = new AccountJsonRepository(@"../../../../MiniBank.Tests/Data/Accounts.json");
+            var actualAccount = new Account()
+            {
+                Id = 3,
+                Iban = "GE91SB4412587693211000",
+                Currency = "GEL",
+                Balance = 3500,
+                CustomerId = 2,
+                Name = ""
+            };
+            
+            //Act
+            var expectedAccount = accountRepository.GetAccount(3);
+            //Assert
+            Assert.NotNull(actualAccount);
+            Assert.Equal(expectedAccount.Id, actualAccount.Id);
+            Assert.Equal(expectedAccount.Iban, actualAccount.Iban);
+            Assert.Equal(expectedAccount.Currency, actualAccount.Currency);
+            Assert.Equal(expectedAccount.Balance, actualAccount.Balance);
+            Assert.Equal(expectedAccount.CustomerId, actualAccount.CustomerId);
+            Assert.Equal(expectedAccount.Name, actualAccount.Name);
+        }
+        [Fact]
+        public void Delete_Account_with_specific_Id() 
+        {
+            var accountRepository = new AccountJsonRepository(@"../../../../MiniBank.Tests/Data/Accounts.json");
+            accountRepository.Delete(2);
+            var expected = 3;
+            string jsonString = File.ReadAllText(@"../../../../MiniBank.Tests/Data/Accounts.json");
+
+            // Parse the JSON as JArray
+            JArray jsonArray = JArray.Parse(jsonString);
+
+            // Get the count of objects
+            int numberOfObjects = jsonArray.Count;
+            Assert.Equal(expected, numberOfObjects);
+        }
+}
+}
